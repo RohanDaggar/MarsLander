17d4164253148b148bd350826037116e531ed313
RohanDaggar <dagzthegreat@gmail.com>
2022-08-19 18:33:47 +0100

    better approximation

--- spring.py
+++ spring.py
@@ -1,45 +1,49 @@
-# uncomment the next line if running in a notebook
-# %matplotlib inline
-import numpy as np
-import matplotlib.pyplot as plt
+    # uncomment the next line if running in a notebook
+    # %matplotlib inline
+    import numpy as np
+    import matplotlib.pyplot as plt
 
-# mass, spring constant, initial position and velocity
-m = 1
-k = 1
-x = 0
-v = 1
+    # mass, spring constant, initial position and velocity
+    m = 1
+    k = 1
+    x = 1
+    v = 0
 
-# simulation time, timestep and time
-t_max = 100
-dt = 0.1
-t_array = np.arange(0, t_max, dt)
+    # simulation time, timestep and time
+    t_max = 10
+    dt = 0.0001 #! inital 0.1 is too small, values increase 
+    t_array = np.arange(0, t_max, dt)
 
-# initialise empty lists to record trajectories
-x_list = []
-v_list = []
+    # initialise empty lists to record trajectories
+    x_list = []
+    v_list = []
 
-# Euler integration
-for t in t_array:
+    # Euler integration
+    for t in t_array:
 
-    # append current state to trajectories
-    x_list.append(x)
-    v_list.append(v)
+        # append current state to trajectories
+        x_list.append(x)
+        v_list.append(v)
 
-    # calculate new position and velocity
-    a = -k * x / m
-    x = x + dt * v
-    v = v + dt * a
+        # calculate new position and velocity
+        a = -k * x / m
+        x = x + dt * v
+        v = v + dt * a
 
-# convert trajectory lists into arrays, so they can be sliced (useful for Assignment 2)
-x_array = np.array(x_list)
-v_array = np.array(v_list)
+    # convert trajectory lists into arrays, so they can be sliced (useful for Assignment 2)
+    x_array = np.array(x_list)
+    v_array = np.array(v_list)
 
-# plot the position-time graph
-plt.figure(1)
-plt.clf()
-plt.xlabel('time (s)')
-plt.grid()
-plt.plot(t_array, x_array, label='x (m)')
-plt.plot(t_array, v_array, label='v (m/s)')
-plt.legend()
-plt.show()
+    # plot the position-time graph
+    plt.figure(1)
+    plt.clf()
+    plt.xlabel('time (s)')
+    plt.grid()
+    plt.plot(t_array, x_array, label='x (m)')
+    plt.plot(t_array, v_array, label='v (m/s)')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "main":
+    run()
