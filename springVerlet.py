5a4cf506bfcaf63b6ddc0402f7287f7d377b20b0
rohandaggar <dagzthegreat@gmail.com>
2022-08-20 00:33:57 +0100

    fixed verlet inital vel

--- springVerlet.py
+++ springVerlet.py
@@ -7,12 +7,12 @@ def run():
     # mass, spring constant, initial position and velocity
     m = 1
     k = 1
-    x = 1
-    v = 0
+    x = 0
+    v = 1
 
     # simulation time, timestep and time
-    t_max = 10
-    dt = 0.0001 #! inital 0.1 is too small, values increase 
+    t_max = 1000
+    dt = 0.1 #! inital 0.1 is too small, values increase 
     t_array = np.arange(0, t_max, dt)
 
     # initialise empty lists to record trajectories
@@ -20,20 +20,22 @@ def run():
     v_list = []
 
     # Verlet integration
-    x_list.append(x)
-    v_list.append(v)
     for t in t_array:
         
         # append current state to trajectories
         x_list.append(x)
         v_list.append(v)
-
-        # calculate new position and velocity using a as x(t-1) and b as v(t-1)
-        a=x
-        x = 2 * x - x_list[-2] + dt**2 * -k * x / m
-        v = (x-a)/dt
-    x_list.pop(0)
-    v_list.pop(0)
+        
+        # This is for the first operation of the verlet to utilise initial velocity
+        if t == 0:
+            a = x
+            x = x + v*dt + 0.5 * dt**2 * -k * x
+            v = (x-a)/dt
+        # else calculate new position and velocity
+        else:
+            a=x
+            x = 2 * x - x_list[-2] + dt**2 * -k * x / m
+            v = (x-a)/dt
         
 
     # convert trajectory lists into arrays, so they can be sliced (useful for Assignment 2)
@@ -53,4 +55,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-
