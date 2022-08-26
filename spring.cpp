2f5e55ca235b9e9036bced1870833dfcd413256e
RohanDaggar <dagzthegreat@gmail.com>
2022-08-26 20:46:09 +0100

    formatting

--- spring.cpp
+++ spring.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
   // declare variables
   double m, k, x, v, t_max, dt, t, a;
@@ -21,7 +22,8 @@ int main() {
   dt = 0.1;
 
   // Euler integration
-  for (t = 0; t <= t_max; t = t + dt) {
+  for (t = 0; t <= t_max; t = t + dt)
+  {
 
     // append current state to trajectories
     t_list.push_back(t);
@@ -32,17 +34,20 @@ int main() {
     a = -k * x / m;
     x = x + dt * v;
     v = v + dt * a;
-
   }
 
   // Write the trajectories to file
   ofstream fout;
   fout.open("trajectories.txt");
-  if (fout) { // file opened successfully
-    for (int i = 0; i < t_list.size(); i = i + 1) {
+  if (fout)
+  { // file opened successfully
+    for (int i = 0; i < t_list.size(); i = i + 1)
+    {
       fout << t_list[i] << ' ' << x_list[i] << ' ' << v_list[i] << endl;
     }
-  } else { // file did not open successfully
+  }
+  else
+  { // file did not open successfully
     cout << "Could not open trajectory file for writing" << endl;
   }
 
